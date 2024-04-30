f5914baf8fb612ac1c19d6a5a4178f686365e0e3
Cody Walenciak <codywalenciak@gmail.com>
2024-04-30 10:59:31 -0500

    Fix syntax of getNumberOfDepartments

--- ES6_basic/12-createReportObject.js
+++ ES6_basic/12-createReportObject.js
@@ -3,11 +3,7 @@ export default function createReportObject(employeesList) {
   const employeesObj = {
     allEmployees: { ...employeesList },
     getNumberOfDepartments(employeesList) {
-      let count = 0;
-      for (const department in employeesList) {
-        count += 1;
-      }
-      return count;
+      return Object.keys(employeesList).length;
     },
   };
   return employeesObj;
