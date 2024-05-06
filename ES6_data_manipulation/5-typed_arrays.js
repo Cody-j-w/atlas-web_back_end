434bb263ecafa31f6658a48d49257d08bcc08264
Cody Walenciak <codywalenciak@gmail.com>
2024-05-06 14:42:46 -0500

    Refactor task 5-typed_arrays

--- ES6_data_manipulation/5-typed_arrays.js
+++ ES6_data_manipulation/5-typed_arrays.js
@@ -1,5 +1,9 @@
 export default function createInt8TypedArray(len, pos, val) {
   const newBuffer = new ArrayBuffer(len);
-  new DataView(newBuffer).setInt8(pos, val);
-  return newBuffer;
+  if (pos >= len) {
+    throw new Error("Position outside range");
+  }
+  let bufferView = new DataView(newBuffer);
+  bufferView.setInt8(pos, val);
+  return bufferView;
 }
