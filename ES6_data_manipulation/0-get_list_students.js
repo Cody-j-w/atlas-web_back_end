07aa01da3beb142ba411247662998886b0f09071
Cody Walenciak <codywalenciak@gmail.com>
2024-05-06 10:07:11 -0500

    Add task 0-get_list_students

---
+++ ES6_data_manipulation/0-get_list_students.js
@@ -0,0 +1,9 @@
+export default function getListStudents() {
+  const s1 = { id: 1, name: 'Guillaume', location: 'San Francisco' };
+  const s2 = { id: 2, name: 'James', location: 'Columbia' };
+  const s3 = { id: 5, name: 'Serena', location: 'San Francisco' };
+
+  const studentArray = [s1, s2, s3];
+
+  return studentArray;
+}
