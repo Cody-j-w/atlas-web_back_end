36e437fd16e5c35d4981543517dc935a03305fc2
Cody Walenciak <codywalenciak@gmail.com>
2024-05-06 10:43:08 -0500

    Fix studentArray structure

--- ES6_data_manipulation/0-get_list_students.js
+++ ES6_data_manipulation/0-get_list_students.js
@@ -1,7 +1,7 @@
 export default function getListStudents() {
-  const s1 = { id: 1, name: 'Guillaume', location: 'San Francisco' };
-  const s2 = { id: 2, name: 'James', location: 'Columbia' };
-  const s3 = { id: 5, name: 'Serena', location: 'San Francisco' };
+  const s1 = { firstName: 'Guillaume', id: 1, location: 'San Francisco' };
+  const s2 = { firstName: 'James', id: 2, location: 'Columbia' };
+  const s3 = { firstName: 'Serena', id: 5, location: 'San Francisco' };
 
   const studentArray = [s1, s2, s3];
 
