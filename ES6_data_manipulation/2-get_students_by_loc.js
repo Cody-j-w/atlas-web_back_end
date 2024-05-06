e13f803ba27f9ed3b6248db2f31e03d63f6925d5
Cody Walenciak <codywalenciak@gmail.com>
2024-05-06 11:08:17 -0500

    Syntax fix

--- ES6_data_manipulation/2-get_students_by_loc.js
+++ ES6_data_manipulation/2-get_students_by_loc.js
@@ -1,5 +1,3 @@
 export default function getStudentsByLocation(arr, city) {
-    if (Array.isArray(arr)) {
-        return arr.filter((student) => student.location === city);
-    }
+  return arr.filter((student) => student.location === city);
 }
