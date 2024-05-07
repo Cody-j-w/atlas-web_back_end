3dbf4a15f91825edda8200b88b61fa14293a2b44
Cody Walenciak <codywalenciak@gmail.com>
2024-05-07 14:21:23 -0500

    Style fixes

--- ES6_promise/2-then.js
+++ ES6_promise/2-then.js
@@ -2,6 +2,5 @@ export default function handleResponseFromAPI(promise) {
   return promise.then(() => ({
     body: 'success',
     status: 200,
-  })).catch(() => Error()).finally(() => 
-    console.log('Got a response from the API'));
+  })).catch(() => Error()).finally(() => console.log('Got a response from the API'));
 }
