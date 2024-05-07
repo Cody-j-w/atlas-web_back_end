1c971d24634b692a4a8043eaaa0fba5371c4f255
Cody Walenciak <codywalenciak@gmail.com>
2024-05-07 11:21:00 -0500

    Add check for non-string param

--- ES6_data_manipulation/8-clean_set.js
+++ ES6_data_manipulation/8-clean_set.js
@@ -1,5 +1,8 @@
-export default function cleanSet(set, str) {
+export default function cleanSet(set, str="") {
   let resStr = '';
+  if (typeof str !== "string") {
+    return resStr;
+  }
   set.forEach((x) => {
     if (x.slice(0, str.length) === str && str.length !== 0) {
       if (resStr.length !== 0) {
