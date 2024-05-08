88f113cbff7589a0a9ee68a16bf978fc8e866846
Cody Walenciak <codywalenciak@gmail.com>
2024-05-08 10:00:39 -0500

    Swap .all() for .allSettled()

--- ES6_promise/6-final-user.js
+++ ES6_promise/6-final-user.js
@@ -1,7 +1,23 @@
-import uploadPhoto from './5-photo-reject.js';
-import signUpUser from './4-user-promise.js';
+import uploadPhoto from './5-photo-reject';
+import signUpUser from './4-user-promise';
 
-export default function handleProfileSignup(firstName, lastName, fileName) {
-  return Promise.all([uploadPhoto(fileName), signUpUser(firstName, lastName)]).then(() => {
+export default function handleProfileSignup(first, last, file) {
+  const signupData = [];
+  return Promise.allSettled([uploadPhoto(file), signUpUser(first, last)]).then((results) => {
+    results.forEach((result) => {
+      let data = {};
+      if (result.status === 'fulfilled') {
+        data = {
+          status: result.status,
+          value: result.value,
+        };
+      } else {
+        data = {
+          status: result.status,
+          value: result.reason,
+        };
+      }
+      signupData.push(data);
+    });
   }).catch(() => Error);
 }
