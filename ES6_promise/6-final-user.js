710c0d29cecdf076f7d6ba376249aa5db0640296
Cody Walenciak <codywalenciak@gmail.com>
2024-05-08 11:43:17 -0500

    Style fixes

--- ES6_promise/6-final-user.js
+++ ES6_promise/6-final-user.js
@@ -1,33 +1,33 @@
-import uploadPhoto from './5-photo-reject.js';
-import signUpUser from './4-user-promise.js';
+import uploadPhoto from './5-photo-reject';
+import signUpUser from './4-user-promise';
 
 export default async function handleProfileSignup(first, last, file) {
-  let data = [];
+  const data = [];
   try {
     const userResponse = await signUpUser(first, last);
     data.push({
-        status: 'fulfilled',
-        value: userResponse
-    })
-  } catch(error) {
+      status: 'fulfilled',
+      value: userResponse,
+    });
+  } catch (error) {
     data.push({
-        status: 'fulfilled',
-        value: userResponse
-    })
+      status: 'fulfilled',
+      value: userResponse,
+    });
   }
 
   try {
     const photoResponse = await uploadPhoto(file);
     data.push({
-        status: 'fulfilled',
-        value: photoResponse
-    })
-  } catch(error) {
+      status: 'fulfilled',
+      value: photoResponse,
+    });
+  } catch (error) {
     data.push({
-        status: 'rejected',
-        value: `Error: ${file} cannot be processed`
-    })
+      status: 'rejected',
+      value: `Error: ${file} cannot be processed`,
+    });
   }
-  
+
   return data;
 }
