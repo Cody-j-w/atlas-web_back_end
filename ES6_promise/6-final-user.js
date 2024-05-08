8ed498b810204f889c26f1c38a827feb0e87e10b
Cody Walenciak <codywalenciak@gmail.com>
2024-05-08 11:42:18 -0500

    Refactored task 6-final-user

--- ES6_promise/6-final-user.js
+++ ES6_promise/6-final-user.js
@@ -1,23 +1,33 @@
-import uploadPhoto from './5-photo-reject';
-import signUpUser from './4-user-promise';
+import uploadPhoto from './5-photo-reject.js';
+import signUpUser from './4-user-promise.js';
 
-export default function handleProfileSignup(first, last, file) {
-  return Promise.allSettled([uploadPhoto(file), signUpUser(first, last)]).then((results) => {
-    const data = [];
-    results.forEach((result) => {
-      if (result.status === 'fulfilled') {
-        data.push({
-          status: result.status,
-          value: result.value,
-        });
-      } else {
-        data.push({
-          status: result.status,
-          value: `Error: ${file} cannot be processed`,
-        });
-      }
-    });
-    console.log(data);
-    return data;
-  });
+export default async function handleProfileSignup(first, last, file) {
+  let data = [];
+  try {
+    const userResponse = await signUpUser(first, last);
+    data.push({
+        status: 'fulfilled',
+        value: userResponse
+    })
+  } catch(error) {
+    data.push({
+        status: 'fulfilled',
+        value: userResponse
+    })
+  }
+
+  try {
+    const photoResponse = await uploadPhoto(file);
+    data.push({
+        status: 'fulfilled',
+        value: photoResponse
+    })
+  } catch(error) {
+    data.push({
+        status: 'rejected',
+        value: `Error: ${file} cannot be processed`
+    })
+  }
+  
+  return data;
 }
