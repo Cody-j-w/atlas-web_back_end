a7911732f8ce7422401ca31bd88f2a3f597a3ed8
Cody Walenciak <codywalenciak@gmail.com>
2024-08-26 10:36:02 -0500

    Add concat function

---
+++ python_variable_annotations/1-concat.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python3
+
+"""
+Module containing string concatenation function
+"""
+
+
+def concat(a: str, b: str) -> str:
+    """
+    accept two strings and concatenate the two. Return the new string.
+    """
+
+    return a + b
