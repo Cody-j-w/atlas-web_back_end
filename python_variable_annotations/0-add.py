ee52f8b9840f423ef5a4ced2b7e21405cd3acc7a
Cody Walenciak <codywalenciak@gmail.com>
2024-08-26 10:18:45 -0500

    Add module docstring

--- python_variable_annotations/0-add.py
+++ python_variable_annotations/0-add.py
@@ -1,4 +1,9 @@
 #!/usr/bin/env python3
+
+"""
+Module containing add function
+"""
+
 def add(a: float, b: float) -> float:
     """
     accepts two floats and adds them together, returning a float
