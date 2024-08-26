9782ac60c62fb7f878c85c875fc13d9b2fffe4ab
Cody Walenciak <codywalenciak@gmail.com>
2024-08-26 11:06:39 -0500

    Add function to stringify a float

---
+++ python_variable_annotations/3-to_str.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python3
+
+"""
+module containing function that returns a stringified float
+"""
+
+
+def to_str(n: float) -> str:
+    """
+    accepts a float and returns the string representation of that float
+    """
+
+    return str(n)
