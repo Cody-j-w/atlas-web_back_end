ad80aeb7e099fc9d090db2a65988805281e2428a
Cody Walenciak <codywalenciak@gmail.com>
2024-08-27 10:06:24 -0500

    Modify param annotation: float -> int

--- python_async_function/0-basic_async_syntax.py
+++ python_async_function/0-basic_async_syntax.py
@@ -3,7 +3,7 @@ import random
 import asyncio
 
 
-async def wait_random(max_delay: float=10) -> float:
+async def wait_random(max_delay: int=10) -> float:
     rand:float = random.uniform(0, max_delay)
     await asyncio.sleep(rand)
     return rand
