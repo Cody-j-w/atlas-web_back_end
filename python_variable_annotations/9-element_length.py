7c01008733de42c2ba35b391508aff39ee75a931
Cody Walenciak <codywalenciak@gmail.com>
2024-08-27 09:41:18 -0500

    Fix documentation

--- python_variable_annotations/9-element_length.py
+++ python_variable_annotations/9-element_length.py
@@ -1,11 +1,16 @@
 #!/usr/bin/env python3
 
 """
-module containing make_multiplier function
+module containing annotated element_length function
 """
 
 from typing import Iterable, Tuple, Sequence
 
 
 def element_length(lst: Iterable[Sequence]) -> Tuple[Sequence, int]:
+    """
+    Accepts an iterable of some kind
+    Returns a tuple
+    """
+
     return [(i, len(i)) for i in lst]
