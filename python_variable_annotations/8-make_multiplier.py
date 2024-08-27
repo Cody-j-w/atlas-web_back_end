471479fbdf14532e7e62462149a46eef23b78d96
Cody Walenciak <codywalenciak@gmail.com>
2024-08-27 09:31:28 -0500

    Add make_multiplier function

---
+++ python_variable_annotations/8-make_multiplier.py
@@ -0,0 +1,20 @@
+#!/usr/bin/env python3
+
+"""
+module containing make_multiplier function
+"""
+
+from typing import Callable
+
+
+def make_multiplier(multiplier: float) -> Callable[[float], float]:
+    """
+    Accept a float, pass it into an internal function, and multiply it
+    Return a float
+    """
+    def multiply(multi: float) -> float:
+        return multi * multiplier
+    return multiply
+
+m = make_multiplier(5)
+print(m(2))
