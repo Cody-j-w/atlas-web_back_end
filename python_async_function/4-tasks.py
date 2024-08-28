fad02591312ce7518785f57e118a76b7df4bc21e
Cody Walenciak <codywalenciak@gmail.com>
2024-08-28 10:43:26 -0500

    Alternate version of 1-concurrent_coroutines that creates Tasks

---
+++ python_async_function/4-tasks.py
@@ -0,0 +1,23 @@
+#!/usr/bin/env python3
+"""
+Module containing function that calls multiple task_wait_random()s
+"""
+import random
+import asyncio
+from typing import List
+task_wait_random = __import__('3-tasks').task_wait_random
+
+
+async def wait_n(n: int, max_delay: int) -> List[float]:
+    """
+    accepts two ints:
+    int n is the number of processes to start
+    int max_delay is the max wait time of those processes
+    returns a list of the wait times
+    """
+    times: List[float] = []
+    tasks = [task_wait_random(max_delay) for _ in range(n)]
+    for task in asyncio.as_completed(tasks):
+        res = await task
+        times.append(res)
+    return times
