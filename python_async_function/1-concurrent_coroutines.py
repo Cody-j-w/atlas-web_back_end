da55f3b80f53deb7e23376d5899019847a97129e
Cody Walenciak <codywalenciak@gmail.com>
2024-08-28 10:06:07 -0500

    Style and documentation fixes

--- python_async_function/1-concurrent_coroutines.py
+++ python_async_function/1-concurrent_coroutines.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python3
 """
-Module containing asyncronous sleep function
+Module containing function that calls multiple wait_random()s
 """
 import random
 import asyncio
@@ -8,9 +8,15 @@ from typing import List
 wait_random = __import__('0-basic_async_syntax').wait_random
 
 
-async def wait_n(n:int, max_delay:int) -> List[float]:
+async def wait_n(n: int, delay: int) -> List[float]:
+    """
+    accepts two ints:
+    int n is the number of processes to start
+    int delay is the max wait time of those processes
+    returns a list of the wait times
+    """
     times: List[float] = []
-    for task in asyncio.as_completed([wait_random(max_delay) for _ in range(n)]):
+    for task in asyncio.as_completed([wait_random(delay) for _ in range(n)]):
         res = await task
         times.append(res)
     return times
