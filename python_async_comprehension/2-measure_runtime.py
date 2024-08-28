c55532c908de247a730815443e798f5cb5e8108f
Cody Walenciak <codywalenciak@gmail.com>
2024-08-28 14:03:35 -0500

    Reformatting

--- python_async_comprehension/2-measure_runtime.py
+++ python_async_comprehension/2-measure_runtime.py
@@ -4,7 +4,7 @@ Module containing function that times the execution time of 4 instances of
 the async_comprehension function
 """
 import random
-from asyncio import gather
+import asyncio
 import time
 async_comprehension = __import__('1-async_comprehension').async_comprehension
 
@@ -14,10 +14,6 @@ async def measure_runtime() -> float:
     compare runtimes of four parallel async functions
     """
     start = time.time()
-    await gather(
-        async_comprehension(),
-        async_comprehension(),
-        async_comprehension(),
-        async_comprehension())
+    await asyncio.gather(async_comprehension(), async_comprehension(), async_comprehension(), async_comprehension())
     stop = time.time()
     return stop - start
