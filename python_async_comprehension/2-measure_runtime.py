863843187ef7dbae228ef25bd96f1abea88108cd
Cody Walenciak <codywalenciak@gmail.com>
2024-08-28 13:46:49 -0500

    Adjust use of gather

--- python_async_comprehension/2-measure_runtime.py
+++ python_async_comprehension/2-measure_runtime.py
@@ -4,7 +4,7 @@ Module containing function that times the execution time of 4 instances of
 the async_comprehension function
 """
 import random
-import asyncio
+from asyncio import gather
 import time
 async_comprehension = __import__('1-async_comprehension').async_comprehension
 
@@ -14,7 +14,7 @@ async def measure_runtime() -> float:
     compare runtimes of four parallel async functions
     """
     start = time.time()
-    await asyncio.gather(
+    await gather(
         async_comprehension(),
         async_comprehension(),
         async_comprehension(),
