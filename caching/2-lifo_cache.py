330294be718c2880d4cafcedefde9c56edf52a05
Cody Walenciak <codywalenciak@gmail.com>
2024-09-03 15:47:35 -0500

    Update lifo_cache

--- caching/2-lifo_cache.py
+++ caching/2-lifo_cache.py
@@ -20,12 +20,14 @@ class LIFOCache(BaseCaching):
         Add item to cache - eject last item if over limit
         """
         if key is not None and item is not None:
-            if len(self.cache_data) == self.MAX_ITEMS:
-                print(f"DISCARD: {self.stack[self.MAX_ITEMS-1]}")
-                del self.cache_data[self.stack[self.MAX_ITEMS-1]]
-                self.stack.pop(self.MAX_ITEMS-1)
+            if len(self.cache_data) >= self.MAX_ITEMS:
+                if key not in self.cache_data:
+                    print(f"DISCARD: {self.stack[len(self.stack)-1]}")
+                    del self.cache_data[self.stack[len(self.stack)-1]]
+                    self.stack.pop(len(self.stack)-1)
             self.cache_data.update({key: item})
             self.stack.append(key)
+            print("Current stack: "+str(self.stack))
 
     def get(self, key):
         """
