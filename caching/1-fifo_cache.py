45c0bc16ba885a10b4fc55d8aa04baf1b1645f51
Cody Walenciak <codywalenciak@gmail.com>
2024-09-03 13:40:29 -0500

    Swap item deletion back to del from pop()

--- caching/1-fifo_cache.py
+++ caching/1-fifo_cache.py
@@ -22,7 +22,7 @@ class FIFOCache(BaseCaching):
         if key is not None and item is not None:
             if len(self.cache_data) == self.MAX_ITEMS:
                 print(f"DISCARD: {self.stack[0]}")
-                self.cache_data.pop(self.stack[0])
+                del self.cache_data[self.stack[0]]
             self.cache_data.update({key: item})
             self.stack.append(key)
 
