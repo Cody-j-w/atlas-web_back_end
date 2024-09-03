c006ed62fb3a292b039919ca786184034042cc47
Cody Walenciak <codywalenciak@gmail.com>
2024-09-03 10:44:35 -0500

    Style fixes

--- caching/0-basic_cache.py
+++ caching/0-basic_cache.py
@@ -14,14 +14,14 @@ class BasicCache(BaseCaching):
         """
         Add item to cache
         """
-        if key != None and item != None:
+        if key is not None and item is not None:
             self.cache_data.update({key: item})
 
     def get(self, key):
         """
         Retrieve the value of the provided key
         """
-        if key == None or key not in self.cache_data:
+        if key is None or key not in self.cache_data:
             return None
         else:
             return self.cache_data[key]
