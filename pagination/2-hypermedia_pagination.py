b9edbaeb45dc28b7a2914e8a3024e61f05646cb2
Cody Walenciak <codywalenciak@gmail.com>
2024-09-04 15:28:41 -0500

    Alter dataset iteration to avoid out of range error

--- pagination/2-hypermedia_pagination.py
+++ pagination/2-hypermedia_pagination.py
@@ -37,8 +37,9 @@ class Server:
         assert page_size > 0
         data = []
         idx = index_range(page, page_size)
+        end = min(idx[1], math.ceil(len(self.dataset()) / page_size))
         if idx[0] < len(self.dataset()):
-            for i in range(idx[0], idx[1]):
+            for i in range(idx[0], end):
                 data.append(self.dataset()[i])
         return data
 
