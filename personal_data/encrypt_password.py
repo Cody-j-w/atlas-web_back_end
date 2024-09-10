6dbbb8109d050373d6c04d58d875139c05ad8532
Cody Walenciak <codywalenciak@gmail.com>
2024-09-10 13:48:30 -0500

    Add password decryption

--- personal_data/encrypt_password.py
+++ personal_data/encrypt_password.py
@@ -10,3 +10,9 @@ def hash_password(password: str) -> bytes:
     """ encode and encrypt a password
     """
     return bcrypt.hashpw(password.encode('utf-8'), bcrypt.gensalt())
+
+
+def is_valid(hashed_password: bytes, password: str) -> bool:
+    """ tell whether a provided password is a valid password
+    """
+    return bcrypt.checkpw(password.encode('utf-8'), hashed_password)
