eb0eae513abea1b56e2bafe9ca83a566d62edb9b
Cody Walenciak <codywalenciak@gmail.com>
2024-09-11 14:45:31 -0500

    Add Forbidden route

--- Basic_authentication/api/v1/views/index.py
+++ Basic_authentication/api/v1/views/index.py
@@ -31,3 +31,9 @@ def unauthorized():
     """ Unauthorized endpoint
     """
     abort(401)
+
+@app_views.route('/forbidden', strict_slashes=False)
+def forbidden():
+    """ Forbidden endpoint
+    """
+    abort(403)
