7ade33baff5a0f4f8bd358f8c8a776a87f91be47
Cody Walenciak <codywalenciak@gmail.com>
2024-09-12 11:23:05 -0500

    Add BasicAuth class

--- Basic_authentication/api/v1/app.py
+++ Basic_authentication/api/v1/app.py
@@ -9,7 +9,10 @@ from flask_cors import (CORS, cross_origin)
 import os
 auth = None
 auth = os.getenv('AUTH_TYPE')
-if auth:
+if auth == 'basic_auth':
+    from api.v1.auth.basic_auth import BasicAuth
+    auth = BasicAuth()
+else:
     from api.v1.auth.auth import Auth
     auth = Auth()
 
---
+++ Basic_authentication/api/v1/auth/basic_auth.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python3
+""" Module of auth methods
+"""
+
+
+class BasicAuth():
+    """ BasicAuth class
+        Contains methods for authenticating users
+    """
