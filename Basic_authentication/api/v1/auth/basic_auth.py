d8c74f2fdd39200bfbafdd74f53ec2d36817e4f4
Cody Walenciak <codywalenciak@gmail.com>
2024-09-12 11:24:33 -0500

    Adjust BasicAuth inheritence

--- Basic_authentication/api/v1/auth/basic_auth.py
+++ Basic_authentication/api/v1/auth/basic_auth.py
@@ -1,9 +1,9 @@
 #!/usr/bin/env python3
 """ Module of auth methods
 """
+from api.v1.auth.auth import Auth
 
-
-class BasicAuth():
+class BasicAuth(Auth):
     """ BasicAuth class
         Contains methods for authenticating users
     """
