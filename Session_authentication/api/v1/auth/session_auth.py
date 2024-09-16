ce88b898dbeb4713396c59f0750dec3fb354c385
Cody Walenciak <codywalenciak@gmail.com>
2024-09-16 09:51:36 -0500

    Add create_session method

--- Session_authentication/api/v1/auth/session_auth.py
+++ Session_authentication/api/v1/auth/session_auth.py
@@ -3,8 +3,20 @@
 """
 from flask import request
 from api.v1.auth.auth import Auth
+import uuid
 
 
 class SessionAuth(Auth):
     """ Session Authorization class
     """
+
+    user_id_by_session_id = {}
+
+    def create_session(self, user_id: str = None) -> str:
+        """ create a session id for a user
+        """
+
+        if user_id is None or type(user_id) is not str:
+            return None
+        session_id = str(uuid.uuid4())
+        user_id_by_session_id.update({session_id: user_id})
