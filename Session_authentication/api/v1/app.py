bf9a17fe4f4787a9f6c52f7982bda1dbd723c006
Cody Walenciak <codywalenciak@gmail.com>
2024-09-16 10:36:13 -0500

    Add cookie check to before_request handler

--- Session_authentication/api/v1/app.py
+++ Session_authentication/api/v1/app.py
@@ -31,11 +31,13 @@ def handle_request():
     """
     handled_paths = ['/api/v1/status/',
                      '/api/v1/unauthorized/',
-                     '/api/v1/forbidden/']
+                     '/api/v1/forbidden/',
+                     '/api/v1/auth_session/login']
     if auth is not None:
         request.current_user = auth.current_user(request)
         if auth.require_auth(request.path, handled_paths) is True:
-            if auth.authorization_header(request) is None:
+            if (auth.authorization_header(request) is None
+                    and auth.session_cookie(request) is None):
                 abort(401)
             if request.current_user is None:
                 abort(403)
