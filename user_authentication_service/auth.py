3703ad336bc4e3c479693bfe6ce2e62abf601e44
Cody Walenciak <codywalenciak@gmail.com>
2024-09-21 13:48:46 -0500

    Restructure get_reset_password_token

--- user_authentication_service/auth.py
+++ user_authentication_service/auth.py
@@ -67,15 +67,16 @@ class Auth:
 
         self._db.update_user(user_id, session_id=None)
 
-    def get_password_reset_token(self, email: str) -> str:
+    def get_reset_password_token(self, email: str) -> str:
         """ generates a password reset token
         """
-
-        user = self._db.find_user_by(email=email)
-        if user is None:
+        try:
+            user = self._db.find_user_by(email=email)
+            reset = _generate_uuid()
+            self._db.update_user(user.id, reset_token=reset)
+            return reset
+        except NoResultFound:
             raise ValueError
-        reset = _generate_uuid()
-        self._db.update_user(user.id, reset_token=reset)
 
 
 def _hash_password(password: str) -> bytes:
