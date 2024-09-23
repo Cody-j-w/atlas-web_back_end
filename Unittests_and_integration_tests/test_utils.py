88ccf0fec4ded7aff84ed9e408bbd451046dc0ab
Cody Walenciak <codywalenciak@gmail.com>
2024-09-23 14:33:25 -0500

    Add exception tests

--- Unittests_and_integration_tests/test_utils.py
+++ Unittests_and_integration_tests/test_utils.py
@@ -18,3 +18,12 @@ class TestAccessNestedMap(unittest.TestCase):
     def test_access_nested_map(self, nest, path, expected):
         """test successful execution of access_nested_map()"""
         self.assertEqual(access_nested_map(nest, path), expected)
+
+    @parameterized.expand([
+        ({}, ("a",)),
+        ({"a": 1}, ("a", "b"))
+    ])
+    def test_access_nested_map_exception(self, nest, path):
+        """test unsuccessful execution of access_nested_map()"""
+        with self.assertRaises(KeyError):
+            access_nested_map(nest, path)
