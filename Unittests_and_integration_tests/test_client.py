89474a8feb5ba439aa47ae35eca30239185f522e
Cody Walenciak <codywalenciak@gmail.com>
2024-09-25 10:11:52 -0500

    Add public_repos_url test

--- Unittests_and_integration_tests/test_client.py
+++ Unittests_and_integration_tests/test_client.py
@@ -4,8 +4,10 @@
 
 import unittest
 import unittest.mock
+from unittest.mock import PropertyMock
 from parameterized import parameterized
 import client
+import utils
 
 
 class TestGithubOrgClient(unittest.TestCase):
@@ -18,6 +20,18 @@ class TestGithubOrgClient(unittest.TestCase):
     ])
     @unittest.mock.patch('client.get_json')
     def test_org(self, url, mock_json):
+        """ test org method
+        """
         test_request = client.GithubOrgClient(url)
         self.assertEqual(test_request.org, mock_json.return_value)
         mock_json.assert_called_once()
+
+    def test_public_repos_url(self):
+        """ test public_repos method
+        """
+        mock_name = 'client.GithubOrgClient._public_repos_url'
+        with unittest.mock.patch(mock_name,
+                                 new_callable=PropertyMock) as mock:
+            mock.return_value = {'payload': 'success'}
+            test_request = client.GithubOrgClient('test')
+            self.assertEqual(test_request._public_repos_url, mock.return_value)
