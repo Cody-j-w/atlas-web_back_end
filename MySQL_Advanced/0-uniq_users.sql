afd14472fe4fa9ee46affc969d9df9d8ac5addb7
Cody Walenciak <codywalenciak@gmail.com>
2024-09-30 13:15:53 -0500

    Fix syntax error

--- MySQL_Advanced/0-uniq_users.sql
+++ MySQL_Advanced/0-uniq_users.sql
@@ -1,8 +1,8 @@
 -- CREATE USERS TABLE
 CREATE TABLE IF NOT EXISTS users (
     id int NOT NULL AUTO_INCREMENT,
-    email string(255) NOT NULL,
-    name string(255),
+    email varchar(255) NOT NULL,
+    name varchar(255),
     UNIQUE(email),
     PRIMARY KEY(id)
-)
+);
