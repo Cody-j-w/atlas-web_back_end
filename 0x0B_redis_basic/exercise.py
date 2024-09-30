41b82929526338541a7f3e6738688741374b6975
Cody Walenciak <codywalenciak@gmail.com>
2024-09-30 10:45:53 -0500

    Add call_history decorator

--- 0x0B_redis_basic/exercise.py
+++ 0x0B_redis_basic/exercise.py
@@ -18,6 +18,19 @@ def count_calls(method: Callable) -> Callable:
     return wrapper
 
 
+def call_history(method: Callable) -> Callable:
+    @wraps(method)
+    def wrapper(*args, **kwargs):
+        key_in = method.__qualname__+":inputs"
+        key_out = method.__qualname__+":outputs"
+        cache = args[0]
+        cache._redis.rpush(key_in, str(args))
+        output = method(*args, **kwargs)
+        cache._redis.rpush(key_out, output)
+        return output
+    return wrapper
+
+
 class Cache:
     """ Redis Cache class
     """
@@ -28,19 +41,23 @@ class Cache:
         self._redis.flushdb()
 
     @count_calls
+    @call_history
     def store(self, data: Union[str, bytes, int, float]) -> str:
         key = str(uuid.uuid4())
         self._redis.set(key, data)
         return key
 
+    @count_calls
     def get(self, key: str, fn: Callable = None):
         if key is not None:
             if fn is not None:
                 return fn(self._redis.get(key))
             return self._redis.get(key)
 
+    @count_calls
     def get_str(self, val: bytes):
         return str(val)
 
+    @count_calls
     def get_int(self, val: bytes):
         return int(val)
