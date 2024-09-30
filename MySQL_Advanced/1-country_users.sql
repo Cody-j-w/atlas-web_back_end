fde7d9865c34df8efdd3f3a61869b4697047013c
Cody Walenciak <codywalenciak@gmail.com>
2024-09-30 13:39:34 -0500

    Add country column to users

---
+++ MySQL_Advanced/1-country_users.sql
@@ -0,0 +1,9 @@
+-- CREATE USERS TABLE
+CREATE TABLE IF NOT EXISTS users (
+    id int NOT NULL AUTO_INCREMENT,
+    email varchar(255) NOT NULL,
+    name varchar(255),
+    country ENUM('US', 'CO', 'TN') DEFAULT 'US',
+    UNIQUE(email),
+    PRIMARY KEY(id)
+);
