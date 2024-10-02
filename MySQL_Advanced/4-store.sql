07942171fee4d155872ef72a7722f3b58bf2fabd
Cody Walenciak <codywalenciak@gmail.com>
2024-10-02 10:54:36 -0500

    Add item order trigger script

---
+++ MySQL_Advanced/4-store.sql
@@ -0,0 +1,12 @@
+-- item/order trigger script
+
+delimiter $$
+CREATE TRIGGER itemorder BEFORE INSERT ON orders
+    FOR EACH ROW
+    BEGIN
+        UPDATE items
+        SET quantity = quantity - NEW.number
+        WHERE name = NEW.item_name;
+    END;
+$$
+delimiter ;
