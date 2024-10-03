85d2f3d925c0ccb4465ada8c410f7e0d562c5b2e
Cody Walenciak <codywalenciak@gmail.com>
2024-10-03 11:42:01 -0500

    email validation trigger

---
+++ MySQL_Advanced/5-valid_email.sql
@@ -0,0 +1,12 @@
+-- item/order trigger script
+
+delimiter $$
+CREATE TRIGGER validator BEFORE UPDATE ON users
+    FOR EACH ROW
+    BEGIN
+        IF NEW.email != OLD.email THEN
+            SET NEW.valid_email = 0;
+        END IF;
+    END;
+$$
+delimiter ;
