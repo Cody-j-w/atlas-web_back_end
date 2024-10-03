a150aa5267faf8efe63d3442342469b5fbe9c3b6
Cody Walenciak <codywalenciak@gmail.com>
2024-10-03 10:14:36 -0500

    Remove imports

--- MySQL_Advanced/2-fans.sql
+++ MySQL_Advanced/2-fans.sql
@@ -1,6 +1,4 @@
 -- script for counting the number of metal fans and grouping them by country
-SOURCE metal_bands.sql
-
 SELECT origin, SUM(fans) AS nb_fans
 FROM metal_bands
 GROUP BY origin
--- MySQL_Advanced/3-glam_rock.sql
+++ MySQL_Advanced/3-glam_rock.sql
@@ -1,6 +1,4 @@
 -- script for listing glam rock bands by lifetime
-SOURCE metal_bands.sql
-
 SELECT band_name, TIMESTAMPDIFF(year, MAKEDATE(formed, 1), IF(split IS NULL, CURDATE(), MAKEDATE(split, 1))) AS lifespan
 FROM metal_bands
 WHERE style LIKE '%Glam rock%'
