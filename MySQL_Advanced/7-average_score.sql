0a09786b74050e312c760e2a91c1c5f7ae94d522
Cody Walenciak <codywalenciak@gmail.com>
2024-10-03 16:39:06 -0500

    Initial commit of average_score stored procedure

---
+++ MySQL_Advanced/7-average_score.sql
@@ -0,0 +1,18 @@
+-- student bonus procedure
+
+
+
+delimiter $$
+CREATE PROCEDURE ComputeAverageScoreForUser(input_id INT)
+    BEGIN
+        DECLARE average FLOAT;
+        SELECT AVG(COALESCE(score)) INTO average
+        FROM corrections
+        WHERE user_id = input_id;
+
+        UPDATE users
+        SET average_score = average
+        WHERE id = input_id;
+    END;
+$$
+delimiter ;
