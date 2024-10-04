766d22c4345421337584a0e93ab6de992a75f560
Cody Walenciak <codywalenciak@gmail.com>
2024-10-04 15:17:40 -0500

    Change how date is read

--- MySQL_Advanced/11-need_meeting.sql
+++ MySQL_Advanced/11-need_meeting.sql
@@ -4,4 +4,4 @@ CREATE VIEW need_meeting AS
 SELECT name
 FROM students
 WHERE (score < 80 AND last_meeting IS NULL)
-OR (score < 80 AND TIMESTAMPDIFF(month, CURDATE(), last_meeting)>= 1)
+OR (score < 80 AND last_meeting < ADDDATE(CURDATE(), INTERVAL -1 MONTH))
