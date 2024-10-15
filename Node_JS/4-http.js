44034498190cdc537c9bb4de465cf93b9ece9530
Cody Walenciak <codywalenciak@gmail.com>
2024-10-15 11:46:37 -0500

    Node HTTP server - basic end point

--- Node_JS/4-http.js
+++ Node_JS/4-http.js
@@ -1,10 +1,12 @@
 const http = require('http');
 
-const server = http.createServer((req, res) => {
+const app = http.createServer((req, res) => {
   res.write('Hello Holberton School!');
   res.end();
 });
 
-server.listen(1245, () => {
+app.listen(1245, () => {
   console.log('listing on 1245!');
 });
+
+module.exports = app;
