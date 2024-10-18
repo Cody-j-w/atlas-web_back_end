7093e529b4d86962c0cc3b2787794d4bdf12a979
Cody Walenciak <codywalenciak@gmail.com>
2024-10-18 11:37:02 -0500

    Async testing

---
+++ unittests_in_js/6-payment_token.test.js
@@ -0,0 +1,11 @@
+const getPaymentTokenFromApi = require('./6-payment_token.js');
+const { expect } = require('chai');
+
+describe('getPaymentTokenFromApi()', () => {
+    it('should return {data: "Successful response from the API" } when successful', (done) => {
+        getPaymentTokenFromApi(true)
+        .then((data) => {
+            done();
+        });
+    });
+});
