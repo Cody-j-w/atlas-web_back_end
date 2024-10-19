e46ada20dfc09e286bfd884f3bbca4ecfc6c2712
Cody Walenciak <codywalenciak@gmail.com>
2024-10-19 13:31:50 -0500

    Swap back to axios

--- unittests_in_js/9-api/api.test.js
+++ unittests_in_js/9-api/api.test.js
@@ -1,9 +1,9 @@
-request = require('request');
+const axios = require('axios');
 const { expect, assert } = require('chai');
 
 describe('Payment API index', () => {
     it('should return a successful status code and the landing message', () => {
-        request('localhost:7865/').then((res) => {
+        axios.get('localhost:7865/').then((res) => {
             assert.equal(res.status, 200);
             assert.equal(res.data, 'Welcome to the payment system');
         });
@@ -12,15 +12,15 @@ describe('Payment API index', () => {
 
 describe('Payment API cart', () => {
     it('should return a successful status code with a valid id', () => {
-        request('localhost:7865/cart/12', (error, response, body) => {
-            assert.equal(response.statusCode, 200);
-            assert.equal(body, 'Payment methods for cart 12');
+        axios.get('localhost:7865/cart/12').then((res) => {
+            assert.equal(res.status, 200);
+            assert.equal(res.data, 'Payment methods for cart 12');
         });
     });
     it('should return a 404 status with an invalid id', () => {
-        axios.get('localhost:7865/cart/twelve', (error, response, body) => {
-            assert.equal(response.statusCode, 404);
-            assert.equal(body, 'Not found');
+        axios.get('localhost:7865/cart/twelve').then((res) => {
+            assert.equal(res.status, 404);
+            assert.equal(res.data, 'Not found');
         })
     })
 });
