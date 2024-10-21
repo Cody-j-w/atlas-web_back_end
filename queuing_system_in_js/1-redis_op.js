95733d5c7402c952e6b74a381960881ea7d0a666
Cody Walenciak <codywalenciak@gmail.com>
2024-10-21 16:32:38 -0500

    Initial commit of queuing system project

---
+++ queuing_system_in_js/1-redis_op.js
@@ -0,0 +1,27 @@
+import { createClient } from "redis";
+import { print } from "redis";
+
+const client = createClient();
+
+client.on('error', (err) => {
+    console.log(`Redis client not connected to the server: ${err}`);
+});
+
+client.connect().then(() => {
+    console.log('Redis client connected to the server');
+    displaySchoolValue('Holberton');
+    setNewSchool('HolbertonSanFrancisco', '100');
+    displaySchoolValue('HolbertonSanFrancisco');
+});
+
+function setNewSchool(schoolName, value) {
+    client.set(schoolName, value).then((res) => {
+        console.log(res);
+    });
+}
+
+function displaySchoolValue(schoolName) {
+    client.get(schoolName).then((res) => {
+        console.log(res);
+    });
+}
